3295a84ed29b458b28382f8c40f6f6edd207663c
pragatimagar <pragatimagar26@gamil.com>
2024-02-26 12:55:19 +0000

    Added new chnages

--- src/test/java/Chapter1.java
+++ src/test/java/Chapter1.java
@@ -3,7 +3,8 @@ public class Chapter1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("New add and commit");
+		System.out.println("New add and commit");
+		System.out.println("New status");
 	}
 
 }
